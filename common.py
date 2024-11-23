050562c3d49bd399e3454237a7b4cbaaeff75128
Wingoal <panwingoal@gmail.com>
2024-11-23 22:31:17 +0800

    1.BugFix: invalid buffering parameter of set_log_file(); 2.NewFeature: (1) Add dict to object function, (2) Add thread-id to log header and add query_log function

--- common.py
+++ common.py
@@ -3,8 +3,11 @@ import os.path
 from datetime import datetime as datetime
 import time
 import json as js
+import threading
+import re
 
 
+g_log_file_name = ''
 g_log_file = None
 start = 0.
 
@@ -66,8 +69,22 @@ def diclist2dic(diclist, keys):
     return dics
 
 
+class DicObj:
+    def __init__(self, dictionary):
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                setattr(self, key, DicObj(value))
+            else:
+                setattr(self, key, value)
+
+
+def dic2obj(dic):
+    return DicObj(dic)
+
+
 def set_log_file(file_name, suffix=None, timestamp = False):
     global g_log_file
+    global g_log_file_name
     log_dir = os.path.dirname(file_name)
     if not log_dir:
         log_dir = './logs'
@@ -83,14 +100,21 @@ def set_log_file(file_name, suffix=None, timestamp = False):
 
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
+    g_log_file_name = os.path.abspath(log_file_name)
+    g_log_file = open(log_file_name, mode='wt+', encoding='utf-8')
+    return g_log_file_name
 
-    g_log_file = open(log_file_name, mode='wt+', encoding='utf-8', buffering=0)
+
+def get_log_file_name():
+    return g_log_file_name
 
 
 def log(*args):
-    print('%s:' % time_str(), *args)
+    current_thread_id = threading.current_thread().ident
+    print('%s[%s]:' % (time_str(), current_thread_id), *args)
     if g_log_file:
-        print('%s:' % time_str(), *args, file=g_log_file)
+        print('%s[%s]:' % (time_str(), current_thread_id), *args, file=g_log_file)
+        g_log_file.flush()
 
 
 def logs(title, *args):
@@ -99,6 +123,31 @@ def logs(title, *args):
     if g_log_file:
         print('%s:' % time_str(), title, file=g_log_file)
         print(*args, file=g_log_file)
+
+
+def query_log(start_time=None, end_time=None, thread_id=None):
+    if g_log_file is None:
+        return ''
+    log_lines = []
+    # 日志格式： 2024-11-23 15:50:10.238[15972]: log text
+    r'b([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]b'
+    pat = r'(?P<log_time>[2]\d{3}-\d{2}-\d{2}\s\d{2}\:\d{2}\:\d{2}.\d{3})\[(?P<log_thread_id>[0-9]+)\]\:'
+    with open(g_log_file_name, 'rt', encoding='utf-8') as f:
+        lines = f.readlines()
+    for line in lines:
+        m = re.match(pat, line)
+        if not m:
+            break
+        log_time = m.group('log_time')
+        log_thread_id = m.group('log_thread_id')
+        if end_time and log_time > end_time:
+            break
+        if start_time and log_time < start_time:
+            continue
+        if thread_id and int(log_thread_id) != thread_id:
+            continue
+        log_lines.append(line)
+    return ''.join(log_lines)
 
 
 def load_json(file_name):
